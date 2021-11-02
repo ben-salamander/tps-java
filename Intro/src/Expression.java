4454a72f1c38075b56bfb2028d4dab54b578ae07
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-02 14:46:49 +0100

    TP1

--- Intro/src/Expression.java
+++ Intro/src/Expression.java
@@ -2,6 +2,6 @@ public class Expression {
     public static void main(String[] args) {
         double nombre1 = 12;
         int nombre2 = 24;
-        int somme = nombre1+nombre2;
+        //int somme = nombre1+nombre2;
     }
 }
---
+++ Intro/src/TP1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TP1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Saisir un diamètre pour le calcul de l'air : ");
+        int diametre = keyboard.nextInt();
+        float surface = (float) (((diametre/2)*(diametre/2))*Math.PI);
+        System.out.printf("La surface du cercle de diamètre %dcm est de %fcm².",diametre,surface);
+    }
+}
