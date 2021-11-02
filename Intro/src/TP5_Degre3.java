4ea9e2a882c82084f9400db53efaf52e2f377763
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-02 15:19:20 +0100

    TP5

---
+++ Intro/src/TP5_Degre3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TP5_Degre3 {
+    public static void main(String[] args) {
+        //Saisies
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Entrez la valeur a : ");
+        int a = keyboard.nextInt();
+        System.out.print("Entrez la valeur b : ");
+        int b = keyboard.nextInt();
+        System.out.print("Entrez la valeur c : ");
+        int c = keyboard.nextInt();
+        System.out.print("Entrez la valeur x : ");
+        double x = keyboard.nextDouble();
+
+        //Calcul
+        double result = ((float)(a+b)/2)*Math.pow(x,3) + Math.pow(a+b,2)*Math.pow(x,2) + a + b + c;
+
+        //Affichage
+        System.out.printf("Valeur du polynôme est : %f",result);
+    }
+}
