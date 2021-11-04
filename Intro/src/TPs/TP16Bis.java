15698fa30b6bdb96d310275de7cc24ca2910dcdc
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-04 15:11:13 +0100

    Réalisation TP19

--- Intro/src/TPs/TP16Bis.java
+++ Intro/src/TPs/TP16Bis.java
@@ -5,22 +5,19 @@ import java.util.stream.Collectors;
 
 public class TP16Bis {
     public static void main(String[] args) {
-        /*
-         *   {
-         *       a:2,
-         *       b:6,
-         *       c:9
-         *   }
-         *
-         * */
+
         String phrase="je suis dans une joie immence, parce que je suis développeur";
         HashMap<Character,Integer> occurences=new HashMap<>();
+
+        int nbLettres = 0;
+        String specialChar = ",;:!ù*^$&'(-_ç)=?./§%µ¨£°+¤~#{[|`@]}'";
         for(int i=0;i<phrase.length();i++){
             if(occurences.get(phrase.charAt(i))==null){
                 occurences.put(phrase.charAt(i),1);
             }else{
                 occurences.replace(phrase.charAt(i),occurences.get(phrase.charAt(i))+1);
             }
+
         }
         System.out.println(occurences);
     }
--- Intro/src/TPs/TP19.java
+++ Intro/src/TPs/TP19.java
@@ -1,7 +1,41 @@
 package TPs;
 
+import java.util.Scanner;
+
 public class TP19 {
     public static void main(String[] args) {
+        //Init
+        Scanner keyboard = new Scanner(System.in);
+        String chaine;
+        StringBuilder reverse = new StringBuilder();
+        int num;
+        boolean neg;
 
+        //Saisie
+        System.out.print("Saisir un nombre à inverser : ");
+        num = keyboard.nextInt();
+
+        //Traitement
+        //Verif positivité
+        neg = num < 0;
+        num = num < 0 ? num * (-1): num;
+
+        //Transo en chaine
+        chaine = String.valueOf(num);
+
+        //Reverse
+        for (int i = 0; i < chaine.length(); i++) {
+            reverse.append(chaine.charAt(chaine.length() - (i+1)));
+        }
+
+        //Affectation a num
+        num = 0;
+        for (int i = 0; i < reverse.length(); i++) {
+            num = (int) (num + Character.getNumericValue(reverse.charAt(reverse.length()-(i+1))) *(Math.pow(10,i)));
+        }
+        num = neg ? num * (-1) : num;
+
+        //Affichage
+        System.out.println(num);
     }
 }
