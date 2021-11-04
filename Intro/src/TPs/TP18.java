808c9917a018f9d53a02952edbf84dfa3a8fc9d5
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-04 11:06:36 +0100

    Réalisation TP18

--- Intro/src/TPs/TP18.java
+++ Intro/src/TPs/TP18.java
@@ -1,4 +1,22 @@
 package TPs;
 
+import java.util.Scanner;
+
 public class TP18 {
+    public static void main(String[] args) {
+
+        //Saisie
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Saisissez une chaine de caractère à renverser : ");
+        String string = keyboard.nextLine();
+
+        //Traitement
+        StringBuilder reverse = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            reverse.append(string.charAt(string.length() - i - 1));
+        }
+
+        //Affichage
+        System.out.println(reverse);
+    }
 }
