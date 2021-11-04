d30423e4b3f519ebc5691dfbd6863073444aeebb
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-04 12:38:27 +0100

    RMAJ TP16bis prof

--- Intro/src/TPs/TP16Bis.java
+++ Intro/src/TPs/TP16Bis.java
@@ -5,62 +5,23 @@ import java.util.stream.Collectors;
 
 public class TP16Bis {
     public static void main(String[] args) {
-        //Saisie
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Saisissez une chaine de caractère à décomposer : ");
-        String string = keyboard.nextLine();
-        //Alphabet
-        HashMap<Integer,Character> alphabet = new HashMap<>();
-        alphabet.put(0,'a');
-        alphabet.put(1,'b');
-        alphabet.put(2,'c');
-        alphabet.put(3,'d');
-        alphabet.put(4,'e');
-        alphabet.put(5,'f');
-        alphabet.put(6,'g');
-        alphabet.put(7,'h');
-        alphabet.put(8,'i');
-        alphabet.put(9,'j');
-        alphabet.put(10,'k');
-        alphabet.put(11,'l');
-        alphabet.put(12,'m');
-        alphabet.put(13,'n');
-        alphabet.put(14,'o');
-        alphabet.put(15,'p');
-        alphabet.put(16,'q');
-        alphabet.put(17,'r');
-        alphabet.put(18,'s');
-        alphabet.put(19,'t');
-        alphabet.put(20,'u');
-        alphabet.put(21,'v');
-        alphabet.put(22,'w');
-        alphabet.put(23,'x');
-        alphabet.put(24,'y');
-        alphabet.put(25,'z');
-        alphabet.put(26,',');
-        alphabet.put(27,'.');
-        alphabet.put(28,'!');
-        alphabet.put(29,'*');
-
-        //Traitement
-        int[] compteurLettre = new int[alphabet.size()];
-        String stringInLower = string.toLowerCase(Locale.ROOT);
-        for (int i=0; i<string.length(); i++) {
-            if (alphabet.containsValue(stringInLower.toCharArray()[i])){
-                for (int j = 0; j < alphabet.size(); j++) {
-                    if (alphabet.get(j) == stringInLower.toCharArray()[i]){
-                        compteurLettre[j]++;
-                    }
-                }
+        /*
+         *   {
+         *       a:2,
+         *       b:6,
+         *       c:9
+         *   }
+         *
+         * */
+        String phrase="je suis dans une joie immence, parce que je suis développeur";
+        HashMap<Character,Integer> occurences=new HashMap<>();
+        for(int i=0;i<phrase.length();i++){
+            if(occurences.get(phrase.charAt(i))==null){
+                occurences.put(phrase.charAt(i),1);
+            }else{
+                occurences.replace(phrase.charAt(i),occurences.get(phrase.charAt(i))+1);
             }
         }
-
-        //Affichage
-        System.out.printf("'%s' est composé de :",string);
-        for (int i = 0; i < compteurLettre.length; i++) {
-            if (compteurLettre[i] != 0){
-                System.out.printf("\n%s : %d",alphabet.get(i),compteurLettre[i]);
-            }
-        }
+        System.out.println(occurences);
     }
 }
