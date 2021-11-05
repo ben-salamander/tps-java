f05fc518ffc83b59be29fa0355c03ba2be81bb3b
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-05 11:21:50 +0100

    Réa TP23

---
+++ Intro/src/TPs/TP23.java
@@ -0,0 +1,45 @@
+package TPs;
+
+import com.sun.source.tree.WhileLoopTree;
+import com.sun.tools.jconsole.JConsoleContext;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class TP23 {
+    public static void main(String[] args) {
+        //Init
+        Scanner keyboard = new Scanner(System.in);
+        String prog="dep";
+        ArrayList<String> progLangue = new ArrayList<>();
+        ArrayList<String> progLangueNotDesire = new ArrayList<>();
+
+        //Saisie
+        //Liste des prog
+        while (true){
+            System.out.print("\nSaisir un nouveau langage de programmation à ajouter à la liste : ");
+            prog = keyboard.nextLine();
+            if (prog.equals("")) break;
+            progLangue.add(prog);
+            System.out.flush();
+            System.out.print(progLangue);
+        }
+
+        //Liste des prog non désirés
+        while (true){
+            System.out.print("\nSaisir un langage de programmation à supprimer à la liste : ");
+            prog = keyboard.nextLine();
+            if (prog.equals("")) break;
+            progLangueNotDesire.add(prog);
+            System.out.flush();
+            System.out.print(progLangueNotDesire);
+        }
+
+        //Suppression des langages à enlever
+        for (String programme:progLangueNotDesire) {
+            while (progLangue.contains(programme)) progLangue.remove(programme);
+        }
+        System.out.print(progLangue);
+    }
+}
