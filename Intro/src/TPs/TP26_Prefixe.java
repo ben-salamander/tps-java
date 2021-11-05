319ee97433e0fab1281c506642893b55fb4f1437
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-05 21:36:55 +0100

    MRéa TP26

---
+++ Intro/src/TPs/TP26_Prefixe.java
@@ -0,0 +1,22 @@
+package TPs;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class TP26_Prefixe {
+    //Methodes
+    static boolean isPrefixe (String mot, String prefixe){
+        return mot.substring(0, prefixe.length()).equals(prefixe);
+    }
+
+    //MAIN
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Entrez un mot : ");
+        String mot = keyboard.nextLine();
+        System.out.println("Entrez un prefixe : ");
+        String prefixe = keyboard.nextLine();
+
+        System.out.printf("%s %s un prefixe de %s",prefixe,isPrefixe(mot,prefixe)?"est":"n'est pas",mot);
+    }
+}
