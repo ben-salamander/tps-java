8ba581157ef5be556bddcb64ce16aa40b379ad68
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-11-08 15:25:23 +0100

    RBloc affichage

--- Intro/src/TPSs_POO/TP2_IMC.java
+++ Intro/src/TPSs_POO/TP2_IMC.java
@@ -2,50 +2,48 @@ package TPSs_POO;
 
 public class TP2_IMC {
     //Classes
-    static class Patient{
+    static class Patient {
         //Attributs
         private double poids;
         private double taille;
 
         //Constructor
-        public Patient(double poids, double taille){
+        public Patient(double poids, double taille) {
             this.poids = poids;
             this.taille = taille;
         }
 
-        //Get set poids
+        //Get poids
         public double getPoids() {
             return poids;
         }
 
-        public void setPoids(double poids) {
-            this.poids = poids;
-        }
-
-        //Get set taille
+        //Get taille
         public double getTaille() {
             return taille;
         }
 
-        public void setTaille(double taille) {
-            this.taille = taille;
-        }
-
         //Méthodes
-        public double imc(){
-            return this.poids/(Math.pow(this.taille,2));
+        public double imc() {
+            return this.poids / (Math.pow(this.taille, 2));
         }
 
-        public void afficher(){
-            System.out.printf("Poids du patient : %f",this.poids);
-            System.out.printf("\nTaille du patient : %f",this.taille);
-            System.out.printf("\nIMC du patient : %f",this.imc());
+        public void afficher() {
+            System.out.printf("""
+                    --------Patient----------
+                    Poids : %.2f kg
+                    Taille : %.2f cm
+                    IMC : %f
+                    -------------------------
+                    """, this.poids, this.taille, this.imc());
         }
     }
 
     //MAIN
     public static void main(String[] args) {
-        Patient patient = new Patient(78.5,1.74);
+        Patient patient = new Patient(78.5, 1.74);
         patient.afficher();
+        System.out.printf("Poids du patient : %.2f kg\n", patient.getPoids());
+        System.out.printf("Taille du patient : %.2f cm\n", patient.getTaille());
     }
 }
